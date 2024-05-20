9ffa81d4646fea8543a88574a747f97f20613c43
raulperesgoes <raulperesgoes@gmail.com>
2024-05-20 19:34:09 -0300

    create service application

--- src/main/java/com/rpg/dummycommerce/stores/application/controller/StoreController.java
+++ src/main/java/com/rpg/dummycommerce/stores/application/controller/StoreController.java
@@ -1,61 +1,37 @@
 package com.rpg.dummycommerce.stores.application.controller;
 
+import com.rpg.dummycommerce.stores.application.service.StoreService;
 import com.rpg.dummycommerce.stores.domain.dto.StoreDto;
 import com.rpg.dummycommerce.stores.domain.model.StoreModel;
-import com.rpg.dummycommerce.stores.domain.repository.StoreRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 public class StoreController {
 
     @Autowired
-    StoreRepository storeRepository;
+    StoreService storeService;
 
     @PostMapping("/stores")
     public ResponseEntity<StoreModel> saveStore(@RequestBody StoreDto storeDto) {
-        var storeModel = new StoreModel();
-        BeanUtils.copyProperties(storeDto, storeModel);
-        storeModel.setDeleted(false);
-        return ResponseEntity.status(HttpStatus.CREATED).body(storeRepository.save(storeModel));
+        return storeService.saveStore(storeDto);
     }
 
     @GetMapping("/stores/{id}")
     public ResponseEntity<Object> getStore(@PathVariable(value="id") UUID id) {
-        Optional<StoreModel> storeO = storeRepository.findById(id);
-        if (storeO.isEmpty() || storeO.get().isDeleted() == true) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Store not found.");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(storeO.get());
+        return storeService.getStore(id);
     }
 
     @PutMapping("/stores/{id}")
     public ResponseEntity<Object> updateStore(@RequestBody StoreDto storeDto, @PathVariable(value="id") UUID id) {
-        Optional<StoreModel> storeO = storeRepository.findById(id);
-        if (storeO.isEmpty() || storeO.get().isDeleted() == true) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Store not found");
-        }
-        var storeModel = new StoreModel();
-        BeanUtils.copyProperties(storeDto, storeModel);
-        return ResponseEntity.status(HttpStatus.OK).body(storeRepository.save(storeModel));
+        return storeService.updateStore(storeDto, id);
     }
 
     @DeleteMapping("/stores/{id}")
     public ResponseEntity<Object> deleteStore(@PathVariable(value = "id") UUID id) {
-        Optional<StoreModel> storeO = storeRepository.findById(id);
-        if (storeO.isEmpty() || storeO.get().isDeleted() == true) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Store not found.");
-        }
-        var storeModel = new StoreModel();
-        BeanUtils.copyProperties(storeO.get(), storeModel);
-        storeModel.setDeleted(true);
-        storeRepository.save(storeModel);
-        return ResponseEntity.status(HttpStatus.OK).body("Store deleted");
+        return storeService.deleteStore(id);
     }
 }
--- src/main/java/com/rpg/dummycommerce/stores/domain/repository/StoreRepository.java
+++ src/main/java/com/rpg/dummycommerce/stores/domain/repository/StoreRepository.java
@@ -1,5 +1,6 @@
 package com.rpg.dummycommerce.stores.domain.repository;
 
+
 import com.rpg.dummycommerce.stores.domain.model.StoreModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
